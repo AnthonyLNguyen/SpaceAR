c8fc565179222bccc4ff3b65c7223a7c3d2dc4ac
Haris Sattar <harissattar41@gmail.com>
2018-05-21 03:56:12 +0000

    No Comments

---
+++ Assets/GyroCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCam : MonoBehaviour {
+
+    private float initialYAngle = 0f;
+    private float appliedGyroYAngle = 0f;
+    private float calibrationYAngle = 0f;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        Application.targetFrameRate = 60;
+        initialYAngle = transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        ApplyGyroRotation();
+        ApplyCalibration();
+    }
+
+    void ApplyGyroRotation()
+    {
+        transform.rotation = Input.gyro.attitude;
+        transform.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
+        transform.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device.
+        appliedGyroYAngle = transform.eulerAngles.y; // Save the angle around y axis for use in calibration.
+    }
+
+    void ApplyCalibration()
+    {
+        transform.Rotate(0f, -calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
+    }
+}
--- Assets/PlanetLocation.cs
+++ Assets/PlanetLocation.cs
@@ -1,32 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlanetLocation : MonoBehaviour {
 
+    public List<GameObject> planets = new List<GameObject>();
+    
     // Use this for initialization
     void Start () {
-        int mercury = 0;
-        int venus = 1;
-        int mars = 2;
-        int jupiter = 3;
-        int saturn = 4;
-        int uranus = 5;
-        int neptune = 6;
-        int sun = 7;
-        int moon = 8;
 
-        int d = 5;
+        String [] planetNames = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto" };
 
-        double[] elements = {48.3313 + 3.24587E-5 * d, 7.0047 + 5.00E-8 * d, 29.1241 + 1.01444E-5 * d, 0.387098, 0.205635 + 5.59E-10 * d, 168.6562 + 4.0923344368 * d,
-                76.6799 + 2.46590E-5 * d, 3.3946 + 2.75E-8 * d, 54.8910 + 1.38374E-5 * d, 0.723330, 0.006773 - 1.302E-9 * d, 48.0052 + 1.6021302244 * d,
-                49.5574 + 2.11081E-5 * d, 1.8497 - 1.78E-8 * d, 286.5016 + 2.92961E-5 * d, 1.523688, 0.093405 + 2.516E-9 * d, 18.6021 + 0.5240207766 * d,
-                100.4542 + 2.76854E-5 * d, 1.3030 - 1.557E-7 * d, 273.8777 + 1.64505E-5 * d };
+        double[] elements = {0.38709927, 0.20563593, 7.00497902, 252.25032350, 77.45779628, 48.33076593,    // mercury
+            0.72333566, 0.00677672, 3.39467605, 181.97909950, 131.60246718, 76.67984255,                    // venus
+            1.00000261, 0.01671123, -0.00001531, 100.46457166, 102.93768193, 0.0,                           // earth bary center
+            1.52371034, 0.09339410, 1.84969142,  -4.55343205, -23.94362959, 49.55953891,                    // mars
+            5.20288700, 0.04838624, 1.30439695,  34.39644051, 14.72847983, 100.47390909,                    // jupiter
+            9.53667594, 0.05386179, 2.48599187,  49.95424423, 92.59887831, 113.66242448,                    // saturn
+            19.18916464, 0.04725744, 0.77263783, 313.23810451, 170.95427630, 74.01692503,                   // uranus
+            30.06992276, 0.00859048, 1.77004347, -55.12002969, 44.96476227, 131.78422574,                   // neptune
+            39.48211675, 0.24882730, 17.14001206, 238.92903833, 224.06891629, 110.30393684};                // pluto
+
+        double[] rates = {0.00000037, 0.00001906, -0.00594749, 149472.67411175, 0.16047689, -0.12534081,    // mercury
+            0.00000390, -0.00004107, -0.00078890, 58517.81538729, 0.00268329, -0.27769418,                  // venus
+            0.00000562, -0.00004392, -0.01294668, 35999.37244981, 0.32327364, 0.0,                          // earth bary center
+            0.00001847, 0.00007882, -0.00813131, 19140.30268499, 0.44441088, -0.29257343,                   // mars
+            -0.00011607, -0.00013253, -0.00183714, 3034.74612775, 0.21252668, 0.20469106,                   // jupiter
+            -0.00125060, -0.00050991, 0.00193609, 1222.49362201, -0.41897216, -0.28867794,                  // saturn
+            -0.00196176, -0.00004397, -0.00242939, 428.48202785, 0.40805281, 0.04240589,                    // uranus
+            0.00026291, 0.00005105, 0.00035372, 218.45945325, -0.32241464, -0.00508664,                     // neptune
+            -0.00031596, 0.00005170, 0.00004818, 145.20780515, -0.04062942, -0.01183482};                   // pluto
+
+
+        Vector3 scl = new Vector3(0.08f, 0.08f, 0.08f);
+        for (int i = 0; i < 9; i++)
+        {
+            if (i == 2)
+            {
+                Vector3 earth = GetPlanetLocation(elements, rates, 2);
+                GameObject planet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                planet.name = "Sun";
+                planets.Add(planet);
+                Vector3 pos = -earth;
+                planets[i].transform.position = pos.normalized * 2;
+                planets[i].transform.localScale = scl;
+            }
+            else
+            {
+                Vector3 earth = GetPlanetLocation(elements, rates, 2);
+                GameObject planet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                Renderer rend = planet.GetComponent<Renderer>();
+                planet.name = planetNames[i];
+                rend.material = new Material(Resources.Load("Mesh" + planet.name + "Material", typeof(Material)) as Material);
+                planets.Add(planet);
+                Vector3 pos = GetPlanetLocation(elements, rates, i) - earth;
+                planets[i].transform.position = pos.normalized;
+                planets[i].transform.localScale = scl;
+            }
+        }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    Vector3 GetPlanetLocation(double [] elements, double [] rates, int planet)
+    {
+        double time = DateTime.Now.Subtract(new DateTime(2000, 1, 1, 12, 0, 0)).TotalSeconds / (60 * 60 * 24 * 365.25 * 100);
+
+        double a = elements[6 * planet + 0] + rates[6 * planet + 0] * time;             // (au) semi_major_axis
+        double e = elements[6 * planet + 1] + rates[6 * planet + 1] * time;             //  ( ) eccentricity
+        double I = elements[6 * planet + 2] + rates[6 * planet + 2] * time;             //  (°) inclination
+        double L = elements[6 * planet + 3] + rates[6 * planet + 3] * time;             //  (°) mean_longitude
+        double omega_bar = elements[6 * planet + 4] + rates[6 * planet + 4] * time;     //  (°) longitude_of_periapsis
+        double OMEGA = elements[6 * planet + 5] + rates[6 * planet + 5] * time;         //  (°) longitude_of_the_ascending_node
+                                                                                        // step 2
+                                                                                        // compute the argument of perihelion, omega, and the mean anomaly, M
+        double omega = omega_bar - OMEGA;
+        double M = L - omega_bar;
+
+        // step 3a
+        // modulus the mean anomaly so that -180° ≤ M ≤ +180°
+        while (M > 180) M -= 360;  // in degrees
+                                   // step 3b
+                                   // obtain the eccentric anomaly, E, from the solution of Kepler's equation
+                                   //   M = E - e*sinE
+                                   //   where e* = 180/πe = 57.29578e
+        double E = M + (e * 180.0 / Math.PI) * Math.Sin(M * Math.PI / 180.0);  // E0
+        for (int i = 0; i < 5; i++)
+        {  // iterate for precision, 10^(-6) degrees is sufficient
+            E = KeplersEquation(E, M, e);
+        }
+
+        // step 4
+        // compute the planet's heliocentric coordinates in its orbital plane, r', with the x'-axis aligned from the focus to the perihelion
+        omega = omega * Math.PI / 180.0;
+        E = E * Math.PI / 180.0;
+        I = I * Math.PI / 180.0;
+        OMEGA = OMEGA * Math.PI / 180.0;
+        double x0 = a * (Math.Cos(E) - e);
+        double y0 = a * Math.Sqrt((1.0 - e * e)) * Math.Sin(E);
+
+        // step 5
+        // compute the coordinates in the J2000 ecliptic plane, with the x-axis aligned toward the equinox:
+        double x = (Math.Cos(omega) * Math.Cos(OMEGA) - Math.Sin(omega) * Math.Sin(OMEGA) * Math.Cos(I)) * x0 + (-Math.Sin(omega) * Math.Cos(OMEGA) - Math.Cos(omega) * Math.Sin(OMEGA) * Math.Cos(I)) * y0;
+        double y = (Math.Cos(omega) * Math.Sin(OMEGA) + Math.Sin(omega) * Math.Cos(OMEGA) * Math.Cos(I)) * x0 + (-Math.Sin(omega) * Math.Sin(OMEGA) + Math.Cos(omega) * Math.Cos(OMEGA) * Math.Cos(I)) * y0;
+        double z = (Math.Sin(omega) * Math.Sin(I)) * x0 + (Math.Cos(omega) * Math.Sin(I)) * y0;
+
+        Vector3 pos = new Vector3((float)x, (float)y, (float)z);
+
+        return pos;
+    }
+
+    double KeplersEquation(double E, double M, double e)
+    {
+        double deltaM = M - (E - (e * 180.0 / Math.PI) * Math.Sin(E * Math.PI / 180.0));
+        double deltaE = deltaM / (1.0 - e * Math.Cos(E * Math.PI / 180.0));
+        return E + deltaE;
+    }
 }
